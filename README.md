9bf58c0c8a07575379da293ce9ebfbe69f24fdbd
Sidney Lemon <sid.lemon3@gmail.com>
2023-02-22 14:54:21 -0700

    file update

---
+++ README.md
@@ -0,0 +1,9 @@
+## CPNT 262: Assignment 3: If This, Then That
+
+Author name; Sidney Lemon
+
+A link to the GH Pages site:
+
+List the author and links to any Attributions for code and/or assets you used that are not your own;
+
+- RE; CPNT 262 class video recordings and daily class notes
--- js/app.js
+++ js/app.js
@@ -27,11 +27,81 @@ const handleSubmit = function(event) {
 
   // Using if, else if and else blocks: assign a meal recommendation to `answer` based on the submitted options for `day` and `meal`.
 
-  // For example: 
-  if (day === 'tue') {
-    answer = 'tacos';
+ 
+  // Tuesday
+  if (day === 'tue' && meal === 'breakfast') {
+    answer = 'Eggs';
   }
-
+  else if (day === 'tue' && meal === 'lunch') {
+    answer = 'Tacos';
+  }
+  else if (day === 'tue' && meal === 'dinner') {
+    answer = 'Chilli';
+  }
+  // Wednesday
+  else if (day === 'wed' && meal === 'breakfast') {
+    answer = 'Toast';
+  }
+  else if (day === 'wed' && meal === 'lunch') {
+    answer = 'Sandwich';
+  }
+  else if (day === 'wed' && meal === 'dinner') {
+    answer = 'Burgers';
+  }
+  // Thursday
+  else if (day === 'thu' && meal === 'breakfast') {
+    answer = 'Fruit';
+  }
+  else if (day === 'thu' && meal === 'lunch') {
+    answer = 'Salad';
+  }
+  else if (day === 'thu' && meal === 'dinner') {
+    answer = 'Pasta';
+  }
+  // Friday
+  else if (day === 'fri' && meal === 'breakfast') {
+    answer = 'Yogurt';
+  }
+  else if (day === 'fri' && meal === 'lunch') {
+    answer = 'Smoothie';
+  }
+  else if (day === 'fri' && meal === 'dinner') {
+    answer = 'Quesadilla';
+  }
+  // Saturday
+  else if (day === 'sat' && meal === 'breakfast') {
+    answer = 'cereal';
+  }
+  else if (day === 'sat' && meal === 'lunch') {
+    answer = 'sushi';
+  }
+  else if (day === 'sat' && meal === 'dinner') {
+    answer = 'Burgers';
+  }
+  // sunday
+  else if (day === 'sun' && meal === 'breakfast') {
+    answer = 'Granola bar';
+  }
+  else if (day === 'sun' && meal === 'lunch') {
+    answer = 'Burrito';
+  }
+  else if (day === 'sun' && meal === 'dinner') {
+    answer = 'Pizza';
+  }
+  // Monday
+  else if (day === 'mon' && meal === 'breakfast') {
+    answer = 'French Toast';
+  }
+  else if (day === 'mon' && meal === 'lunch') {
+    answer = 'Soup';
+  }
+  else if (day === 'mon' && meal === 'dinner') {
+    answer = 'Lasagna';
+  }
+  // Please Select
+  else  {
+    answer = 'Please select both options';
+  }
 
 
   /************************************/
